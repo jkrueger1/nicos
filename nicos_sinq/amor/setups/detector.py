254ee2314017ec822f76635f47a9e379d66fe93c
smathis <stefan.mathis@psi.ch>
2026-02-24 08:47:51 +0100

    SINQ/AMOR: Use SyncDaqSink to increment scan counter
    The right place to do this would probably be the FileWriter sink, but
    that is tricky because the filewriter integration is apparently
    specifically written to NOT support scan settypes. It is probably not
    worth fixing this though, because only AMOR uses the filewriter
    integration at SINQ and the ESS version has evolved substantially. After
    the ESS merger, we should replace the SINQ version entirely by the ESS
    version.
    
    Also changed log level of successfull synchronization.
    
    Change-Id: Idd6f6c35e0d7c745079f8da58323f20c6a869f3d
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/38583
    Reviewed-by: Stefan Mathis <stefan.mathis@psi.ch>
    Tested-by: Stefan Mathis <stefan.mathis@psi.ch>
    Reviewed-by: Jochen Stahn <jochen.stahn@psi.ch>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>

--- nicos_sinq/amor/setups/detector.py
+++ nicos_sinq/amor/setups/detector.py
@@ -106,7 +106,8 @@ devices = dict(
                             fmtstr = '%4.f',
                             ),
     jbi_liveview = device('nicos.devices.datasinks.LiveViewSink'),
-    synchronize_daq = device('nicos_sinq.amor.devices.datasinks.SyncDaqSink'),
+    synchronize_daq = device('nicos_sinq.amor.devices.datasinks.SyncDaqSink',
+                             settypes={'scan'}),
     )
 
 startupcode = '''
